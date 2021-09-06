6ab837cf21bd418c4f44f7385554cdeaa7653b6e
yaho1231 <hiimjungle@naver.com>
2021-09-06 10:50:38 +0900

    yaho

--- src/components/CalcRoot.tsx
+++ src/components/CalcRoot.tsx
@@ -1,56 +1,59 @@
-import { Button } from '@material-ui/core';
+import { Button, Slider, TextField, Typography } from '@material-ui/core';
 import React from 'react';
 import { useState } from 'react';
 import InputBox from './InputBox';
+import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
+
 
 function CalcRoot():JSX.Element {
   const [result,setResult]=useState(0);
+  const [rightnumber,setrightnumber]=useState(0);
+  const [leftnumber,setleftnumber]=useState(0);
   function add(temp:number){
-    setResult(result+temp);
+    setResult(leftnumber+temp);
   }
   function minus(temp:number){
-    setResult(result-temp);
+    setResult(leftnumber-temp);
   }
   function multiplication(temp:number){
-    setResult(result*temp);
+    setResult(leftnumber*temp);
   }
   function division(temp:number){
-    setResult(result/temp);
+    setResult(leftnumber/temp);
   }
   function clear(){
-    setResult(result*0);
+    setResult(0);
+  }
+  function handleleftnumber(e:any){
+    setleftnumber(Number(e.target.value));
+
+  }function handlerightnumber(e:any){
+    setrightnumber(Number(e.target.value));
   }
   return (
     <div>
       <h1>계산기</h1>
+      <TextField id="outlined-basic" label="LeftNumber" variant="outlined" value={leftnumber} onChange={handleleftnumber}/>
+      <TextField id="filled-basic" label="RightNumber" variant="filled" value={rightnumber} onChange={handlerightnumber}/>
+    <h1></h1>
       <Button variant="contained" onClick={()=>{
-        add(5);
-      }}>plus(+5)</Button>
+        add(rightnumber);
+      }}>plus</Button>
   <Button variant="contained" color="primary" onClick={()=>{
-        minus(5);
-      }}>
-  minus(-5)
-  </Button>
+        minus(rightnumber);
+      }}>minus</Button>
+  <h1></h1>
   <Button variant="outlined" color="primary" onClick={()=>{
-        multiplication(2);
-      }}>
-  multiplication(X2)
-  </Button>
+        multiplication(rightnumber);
+      }}>multiplication</Button>
   <Button variant="outlined" color="secondary" onClick={()=>{
-        division(2);
-      }}>
-  division(%2)
-  </Button>
+        division(rightnumber);
+      }}>division</Button>
   <Button variant="outlined" color="primary" onClick={()=>{
         clear();
-      }}>
-  clear(0)
-  </Button>
+      }}>clear(0)</Button>
     <InputBox result={result}/>
-
     </div>
   )
 }
-
-
 export default CalcRoot;
